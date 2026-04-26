d6dac0dae650261bc744a0f1f0a8c7c14a175e7b
rr7121564-source <rr7121564@gmail.com>
2026-04-26 07:13:13 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-echo "🚀 Starting Telegram C++ API Server..."
+
+# Folder create karna zaruri hai
 mkdir -p /app/data
 
-# Local API Server Background me run karna
+echo "🚀 Starting Telegram C++ API Server..."
+# API Server ko background me start karein
 telegram-bot-api --local --api-id="${API_ID}" --api-hash="${API_HASH}" --http-port=8081 --dir=/app/data &
 
-sleep 3
+# Jab tak port 8081 open na ho jaye, wait karein (Maximum 30 seconds)
+echo "⏳ Waiting for Local API Server to wake up on port 8081..."
+timeout 30s bash -c 'until printf "" 2>>/dev/null >/dev/tcp/127.0.0.1/8081; do sleep 1; done'
 
-echo "🚀 Starting Ultra-Fast Bot..."
+echo "✅ Local API Server is UP!"
+echo "🚀 Starting Python Bot..."
 python3 main.py
