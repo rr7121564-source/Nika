f23a2fb6b607aa713c46cdffda0cdd7f4d0eab08
rr7121564-source <rr7121564@gmail.com>
2026-04-26 07:05:39 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-echo "Starting Dummy HTTP Server and Pyrogram Bot..."
-mkdir -p downloads
-python3 main.py
+echo "🚀 Starting Telegram C++ API Server..."
+mkdir -p /app/data
+# Yahan apna API_ID aur API_HASH environment variable me set zarur karna!
+telegram-bot-api --local --api-id="${API_ID}" --api-hash="${API_HASH}" --http-port=8081 --dir=/app/data &
+
+sleep 3
+
+echo "🚀 Starting Ultra-Fast Bot..."
+python3 bot.py
